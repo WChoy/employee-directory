863ae99f2cb52edac455e4ab0e92cd9d32ddd02d
Kevin Pruett <pruett.kevin@gmail.com>
2015-01-05 16:33:55 -0500

    add style

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
-//= require_tree .
+document.addEventListener("DOMContentLoaded", function() {
+  var employees = document.querySelectorAll('li.employee');
+  var i = 0;
+
+  (function animateLoop() {
+    setTimeout(function () {
+      employees[i].classList.add('pop-in');
+      i++;
+      if (i < employees.length ) { animateLoop(i); }
+    }, 150)
+  })();
+
+});
